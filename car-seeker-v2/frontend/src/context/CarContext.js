95921f5da88aaf04b118c213c8ae122c1f7300bd
Kyle Ivan Barreras <KyleIvanTBarreras@yahoo.com>
2024-02-05 16:30:12 -0800

    Car context file code

--- car-seeker-v2/frontend/src/context/CarContext.js
+++ car-seeker-v2/frontend/src/context/CarContext.js
@@ -1,3 +1,32 @@
-import React, { createContext } from 'react'
+import React, { createContext, useReducer } from 'react'
 
 export const CarContext = createContext();
+
+export const carReducer = (state, action) => {
+    switch(action.type)
+    {
+        case 'SET_CARS':
+            return {
+                cars: action.payload
+            } 
+        case 'CREATE_CARS':
+            return {
+                cars: [action.payload, ...state.cars]
+            }
+        default:
+            return state;
+    }
+}
+
+export const carContextProvider = ({children}) => {
+    const [state, dispatch] = useReducer(carReducer, {
+        cars: null
+    });
+
+    return (
+        <carContext.Provider value={{...state, dispatch}}>
+            {children}
+        </carContext.Provider>
+
+    )
+}
